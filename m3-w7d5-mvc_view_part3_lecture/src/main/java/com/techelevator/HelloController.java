beb1a7ce58b0c7ba1bd73ac144761186f92febb2
Joe Erickson <joe@techelevator.com>
2017-10-13 09:00:07 -0400

    Add lecture code for m3-w7d5

---
+++ m3-w7d5-mvc_view_part3_lecture/src/main/java/com/techelevator/HelloController.java
@@ -0,0 +1,19 @@
+package com.techelevator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class HelloController {
+
+	@RequestMapping("/")
+	public String displayGreeting() {
+		
+		return "index";
+	}
+
+	@RequestMapping("/aboutUs")
+	public String showAboutUs() {
+		return "aboutUs";
+	}
+}
