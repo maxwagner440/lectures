43854f281c086be18e88d869edd3d15dec544ba3
Joe Erickson <joe@techelevator.com>
2017-10-31 09:07:38 -0400

    Add lecture code for m4-w10d2

---
+++ m4-w10d2-js-intro-to-jquery-lecture/script.js
@@ -0,0 +1,39 @@
+﻿//1. Write a function that finds box1 
+//      and adds the class "grow"
+function growBox1() { 
+	$("#box1").addClass("grow");
+}
+
+//2. Write a function that finds and
+//      shrinks the blue box
+
+//3. Remove all of the elements inside of a div on the page
+
+//4. Detach all of the divs on the page
+
+//5. Reattach the divs on the page
+
+//6. Change the text for all of the <p> tags inside boxes
+//      to say a message
+
+//7. Change the li to have <b>List Item</b> as their text
+
+//8. Add the .shadow class to the last box
+
+//9. Toggle the .shadow class on any box passed in
+
+//10. Make any input textbox have the .yellow class
+//      to give it a yellow background
+
+//11. Change the value of the textbox to "Hello World"
+
+//12. Now go back and allow a message to be passed in
+
+//13. Append a new list item
+
+//14. Add a new checkbox and textbox
+//      - make sure to change the id
+
+//15. Now make it possible to switch a textbox to a label
+
+//16. Make all the boxes fade out
